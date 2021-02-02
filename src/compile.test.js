c65dcd2194bba5bf63ec2dad66993e125de1ea51
Joe Hicks <github@joehicks.co.uk>
2021-02-02 12:33:41 +0000

    Test for the compilation module

---
+++ src/compile.test.js
@@ -0,0 +1,64 @@
+import compile from "./compile.js"
+
+import fs from "fs"
+
+const instructions = [
+    {
+        id: "INST00000001",
+        process: 1,
+        next: "INST00000002"
+    },
+    {
+        id: "INST00000002",
+        process: 20,
+        next: "INST00000003"
+    },
+    {
+        id: "INST00000003",
+        process: 21,
+        next: "INST00000004"
+    },
+    {
+        id: "INST00000004",
+        process: 2,
+        next: "INST00000005"
+    },
+    {
+        id: "INST00000005",
+        process: 3,
+        next: "INST00000006"
+    },
+    {
+        id: "INST00000006",
+        process: 10,
+        next: "INST00000007"
+    },
+    {
+        id: "INST00000007",
+        process: 101,
+        arguments: [471, "INST00000008"],
+        next: "INST00000009"
+    },
+    {
+        id: "INST00000009",
+        process: 7,
+        next: "INST0000000a"
+    },
+    {
+        id: "INST0000000a",
+        process: 54,
+        next: "INST00000000"
+    },
+    {
+        id: "INST00000008",
+        process: 8,
+        next: "INST00000000"
+    },
+    {
+        id: "INST00000000",
+        process: 0,
+        next: "INSTaaaaaaaa"
+    }
+]
+
+fs.writeFileSync("./newprog.hex", compile(instructions))
