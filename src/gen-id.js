57602edb8358f0f2c322f9a2a453e7288fc9ce30
Joe Hicks <github@joehicks.co.uk>
2021-02-10 21:21:19 +0000

    Ignore some stuff

--- src/gen-id.js
+++ src/gen-id.js
@@ -4,6 +4,7 @@ const id = () => `INST${Math.round((Math.random() * 0xffffffff)).toString(16)}`
 const uniqueId = instructions => {
     let newId = id()
     // Ensure ID is unique
+    // eslint-disable-next-line
     while (!!instructions.find(inst => inst.id === newId)) {
         newId = id()
     }
