81c9bae8c0a993dd6d8612df82be80850a3d1ff2
Joe Hicks <github@joehicks.co.uk>
2021-02-11 13:41:45 +0000

    Added default sequence

--- src/config.js
+++ src/config.js
@@ -2,10 +2,36 @@ const webPort = 44302
 const wsPort = 44202
 const wsType = {
     UPDATE: "update",
+    SAVENODES: "savenodes",
+    NODEUPDATE: "nodeupdate"
 }
+
+const defaultSequence = [
+    {
+        id: "INST00000000",
+        process: 127,
+        arguments: [],
+        next: null,
+        position: {
+            x: 100,
+            y: 100,
+        },
+    },
+    {
+        id: "INST00000001",
+        process: 0,
+        arguments: [],
+        next: null,
+        position: {
+            x: 200,
+            y: 100,
+        },
+    },
+]
 
 export {
     webPort,
     wsPort,
-    wsType
+    wsType,
+    defaultSequence
 }
