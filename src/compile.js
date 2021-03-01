b61e7705d19da9283ffecb9284d62b1d7b8f65ce
Joe Hicks <github@joehicks.co.uk>
2021-03-01 16:14:09 +0000

    Added more validity tests and better output

--- src/compile.js
+++ src/compile.js
@@ -3,29 +3,72 @@ import uniqueId from "./gen-id.js"
 
 // Compile array of objects to sequence code
 
-const compile = instructions => {
+const compile = (instr, dry, mappings) => {
+    // Create a copy of the instructions to work on
+    let instructions = JSON.parse(JSON.stringify(instr))
+    // Check we have one process start and one process complete
+    if (instructions.filter((i) => i.process === 0).length !== 1) {
+        console.error(
+            "Compilation requires exactly one Sequence Complete instruction"
+        )
+        return false
+    }
+    if (instructions.filter((i) => i.process === 127).length !== 1) {
+        console.error(
+            "Compilation requires exactly one Process Start instruction"
+        )
+        return false
+    }
     // Check each instruction for validity
     // Needs:
     // - id & next in correct format
     // - process number, valid
-    console.log("compile start")
+    console.log("Compile start")
+    console.log(instructions)
     for (let i of instructions) {
-        console.log("for loop")
+        //console.log("Checking", i.id)
         if (!i.id || !i.id.match(/^INST[a-f0-9]{8}$/)) {
+            console.error("Invalid ID found: ", i.id)
             return false
         }
-        if (i.process !== 0 && (!i.next || !i.next.match(/^INST[a-f0-9]{8}$/))) {
+        if (
+            i.process !== 0 &&
+            (!i.next || !i.next.match(/^INST[a-f0-9]{8}$/))
+        ) {
+            console.error("Following process missing in", i.id)
             return false
         }
         if (!i.process && i.process !== 0) {
+            console.error("No process number stated in", i.id)
             return false
         }
         if (processes(i.process).id === -1) {
+            console.error(
+                "Invalid process number stated in",
+                i.id,
+                ":",
+                i.process
+            )
             return false
         }
     }
-    
-    console.log("all hood")
+
+    console.log("Initial tests passed")
+    if (dry) return true
+
+    // ===== Start and finish index correction =====
+    // Move start process to start and end to end
+    const endInst = instructions.splice(
+        instructions.findIndex((i) => i.process === 0),
+        1
+    )[0]
+    instructions.push(endInst)
+
+    const startInst = instructions.splice(
+        instructions.findIndex((i) => i.process === 127),
+        1
+    )[0]
+    instructions.unshift(startInst)
 
     // ===== Jump Insertion =====
 
@@ -36,18 +79,20 @@ const compile = instructions => {
     // and add jumps where required
     while (jumpsChanged) {
         jumpsChanged = false
-
         // Iterate through the instruction array
         for (let i = 0; i < instructions.length - 1; i++) {
             // If the next instruction in the array is not the desired next instruction insert a jump
-            if (!!instructions[i + 1] && instructions[i].next !== instructions[i + 1].id) {
+            if (
+                !!instructions[i + 1] &&
+                instructions[i].next !== instructions[i + 1].id
+            ) {
                 const id = uniqueId(instructions)
                 const nextId = instructions[i + 1].id
                 instructions.splice(i + 1, 0, {
                     id: id,
                     process: 100,
                     arguments: [`${instructions[i].next}`],
-                    next: nextId
+                    next: nextId,
                 })
                 instructions[i].next = id
                 // Record that jumps have been added
@@ -55,7 +100,6 @@ const compile = instructions => {
                 break
             }
         }
-        console.log(instructions.length)
     }
 
     // ===== Offset Calculation =====
@@ -64,10 +108,14 @@ const compile = instructions => {
     // Variable to hold cumulative offset
     let offset = 0
 
+    // Object of offset to instruction ids
+    const map = {}
+
     // Iterate through instructions
     for (let inst of instructions) {
         // Store the current offset
         inst.offset = offset
+        map[offset] = inst.id
         // Update the next offset value
         offset += processes(inst.process).length
     }
@@ -82,9 +130,14 @@ const compile = instructions => {
             // Iterate through the arguments
             for (let i = 0; i < inst.arguments.length; i++) {
                 // If the argument is a valid instruction id
-                if (typeof(inst.arguments[i]) === "string" && inst.arguments[i].match(/^INST[a-f0-9]{8}$/)) {
+                if (
+                    typeof inst.arguments[i] === "string" &&
+                    inst.arguments[i].match(/^INST[a-f0-9]{8}$/)
+                ) {
                     // Locate the target instruction, exit if it does not exist
-                    const destination = instructions.find(ins => ins.id === inst.arguments[i])
+                    const destination = instructions.find(
+                        (ins) => ins.id === inst.arguments[i]
+                    )
                     if (!destination) {
                         return false
                     }
@@ -99,8 +152,21 @@ const compile = instructions => {
     // Use the process construct functions to generate sequence code
 
     // Create a buffer of sequence code from the instructions array
-    const seqCode = Buffer.concat(instructions.map(inst => processes(inst.process).construct(...((!!inst.arguments && !!inst.arguments.length) ? inst.arguments : []))))
-
+    const seqCode = Buffer.concat(
+        instructions.map((inst) =>
+            processes(inst.process).construct(
+                ...(!!inst.arguments && !!inst.arguments.length
+                    ? inst.arguments
+                    : [])
+            )
+        )
+    )
+    if (mappings) {
+        return {
+            seq: seqCode,
+            map: map
+        }
+    }
     return seqCode
 }
 
